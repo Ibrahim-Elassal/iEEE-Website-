f171a55b9dd93af05484edaceeda7908528c1cd5
“ibrahim” <ibrahimelassal662@gmail.com>
2022-07-23 23:14:32 +0200

    finalproject

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const FEATURED = document.querySelector(".featured-image");
+const iMAGE = document.querySelector("img");
+let element = document.createElement("caption");
+// let txtElement = document.createTextNode("Earthrise: A photograph of the Earth and parts of the Moon's surface taken by astronaut William Anders in 1968, during the Apollo 8 mission.");
+// let image =  document.querySelector(".featured-image");
+// let element = document.createElement("caption");
+// let txtElement = document.createTextNode("Earthrise: A photograph of the Earth and parts of the Moon's surface taken by astronaut William Anders in 1968, during the Apollo 8 mission.");
+//
+// image.appendchild(element);
+// element.appendchild(txtElement);
+// console.log(image.getAttribute("alt"));
+
+FEATURED.appendChild(element);
+element.innerHTML = "iMAGE.getAttribute("alt")"
+// iMAGE.getAttribute("alt");
+element.appendChild(txtElement);
